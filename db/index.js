4cb8a7d634f64eccfe28731c9321c2d294c9ea45
Gabrielle Rogers <gabirogers23@gmail.com>
2023-01-19 21:40:03 -0600

    added DATABASE_URL

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
+const { DATABASE_URL } = process.env
+
 const { Client } = require("pg"); // imports the pg module
 
-const client = new Client("postgres://localhost:5432/juicebox-dev");
+const client = new Client(DATABASE_URL);
 
 async function createUser({ username, password, name, location }) {
   try {
